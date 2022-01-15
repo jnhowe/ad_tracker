7b6db65ed88c0b829ed178e9ca06184d028aa438
jnhowe <jnhowe@princeton.edu>
2022-01-15 13:48:41 -0400

    final merges

--- scrap.py
+++ scrap.py
@@ -1,15 +1,19 @@
 import pandas as pd
 
 path = 'c:\\Users\Jafar Howe\Desktop\Projects\cars.tt\\ad_tracker\\'
-old_filename = path + 'trinicars_ad_data.csv'
-new_filename = path + 'newest_test_trinicars_ad_data.csv'
+old_filename = path + 'pin_tt_ad_data.csv'
+new_filename = path + 'testing_pin_tt_ad_data.csv'
 
 old_df = pd.read_csv(old_filename)
 new_df = pd.read_csv(new_filename)
 
+# old_df['title'] = old_df['title'].map(lambda name: name[2:-1])
+# print(old_df)
+# old_df.to_csv(path + 'pin_tt_ad_data.csv', mode='w', index=False, header=True)
+
 final_df = new_df.append(old_df, ignore_index=True)
 
-final_df.to_csv(path + '2' + 'testing_trinicars_ad_data.csv', mode='w', index=False, header=True)
+final_df.to_csv(path + '3' + 'pin_tt_ad_data.csv', mode='w', index=False, header=True)
 
 # def count_diff():
 #     filename = ''
